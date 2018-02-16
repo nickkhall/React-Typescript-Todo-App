5fc5fbafb9d5879c89e6a1ff9ef1a4a1ac473e22
nickhallph <nick.hall@yum.com>
2018-02-15 20:18:03 -0600

    fixed eslint stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
 module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: __dirname,
-    filename: 'dist/bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: { presets: [ 'react', 'es2015' ] }
-      },
-      {
-        test: /\.scss|sass$/,
-        loaders: [ 'style-loader', 'css-loader', 'sass-loader' ]
-      },
-      {
-        test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader',
-        query: { presets: [ 'react', 'es2015' ] }
-      }
-    ]
-  },
-  resolve: {
-    extensions: [ '', '.js', '.jsx', '.ts', '.tsx' ]
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
+	entry: './src/index.tsx',
+	output: {
+		path: __dirname,
+		filename: 'dist/bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: { presets: [ 'react', 'es2015' ] }
+			},
+			{
+				test: /\.scss|sass$/,
+				loaders: [ 'style-loader', 'css-loader', 'sass-loader' ]
+			},
+			{
+				test: /\.tsx?$/,
+				loader: 'awesome-typescript-loader',
+				query: { presets: [ 'react', 'es2015' ] }
+			}
+		]
+	},
+	resolve: {
+		extensions: [ '', '.js', '.jsx', '.ts', '.tsx' ]
+	},
+	devServer: {
+		historyApiFallback: true,
+		contentBase: './'
+	}
 };
